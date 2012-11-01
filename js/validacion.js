f5f885fabee75c63672480148ff2165cca354a09
Juan Pablo Martinez Bohorquez <martinez.bohorquez@gmail.com>
2012-11-01 16:20:16 -0500

    Adicion de archivos

---
+++ js/validacion.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+    jQuery.fn.ForceNumericOnly =
+    function()
+    {
+        return this.each(function()
+        {
+            $(this).keydown(function(e)
+            {
+                var key = e.charCode || e.keyCode || 0;
+                // allow backspace, tab, delete, arrows, numbers and keypad numbers ONLY
+                return (
+                    key == 8 || 
+                    key == 9 ||
+                    key == 46 ||
+                    (key >= 37 && key <= 40) ||
+                    (key >= 48 && key <= 57) ||
+                    (key >= 96 && key <= 105));
+            });
+        });
+    };
+
+
+    $("#yourTextBoxName").ForceNumericOnly();
+});
+
+/*
+$(document).ready(function() {
+    $("#txtboxToFilter").keydown(function(event) {
+        // Allow: backspace, delete, tab, escape, and enter
+        if ( event.keyCode == 46 || event.keyCode == 8 || event.keyCode == 9 || event.keyCode == 27 || event.keyCode == 13 || 
+             // Allow: Ctrl+A
+            (event.keyCode == 65 && event.ctrlKey === true) || 
+             // Allow: home, end, left, right
+            (event.keyCode >= 35 && event.keyCode <= 39)) {
+                 // let it happen, don't do anything
+                 return;
+        }
+        else {
+            // Ensure that it is a number and stop the keypress
+            if (event.shiftKey || (event.keyCode < 48 || event.keyCode > 57) && (event.keyCode < 96 || event.keyCode > 105 )) {
+                event.preventDefault(); 
+            }   
+        }
+    });
+});
+*/
